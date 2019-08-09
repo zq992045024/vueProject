a7ec21731af4d6d4d77270f2b2c23bff3b701ba0
yangruifan <yrfdeyouxiang@163.com>
2019-08-09 19:50:13 +0800

    yangruifan

---
+++ src/views/Xqy/index.vue
@@ -0,0 +1,268 @@
+<template>
+    <div class="box">
+        <div class="boa">
+            <img id="poster" src="https://img.piaoniu.com/poster/c946700cbce302591a7e6eb1be76e10855d99eb6.jpg?imageView2/2/w/208/h/280">
+            <div class="boa1">
+                <div class="boa1_1">
+                    2019张杰【未·LIVE】全球巡回演唱会 北京站
+                </div>
+                <div class="boa1_2">
+                    <span>9.8</span>
+                </div>
+                <div class="boa1_4">
+                    <div class="boa1_5">
+                        <p class="boa1_5_1">280</p>
+                        <p class="boa1_5_2">元起</p>
+                    </div>
+                    <div class="boa1_6">
+                       已有14170人购买
+                    </div>
+                </div>
+            </div>
+            <div class="bow">
+                <div class="bow1">
+                    <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAcCAYAAAB2+A+pAAAAAXNSR0IArs4c6QAAAppJREFUSA3Nlk2IjWEUx+/MhGTufNgo2WjKisQCU7KYBQtZ+EjKjIRiNFMTC5nNLFgMiVIWiiUTKzaUnVIsLCQuG4kFWfgsM2S4fv/XOa97n3mfe++Md8qp333Ocz7+5973vl+FQmDlcnkFnIUn8Am+wlM4A4uC8nRLrgtOwWNQ3xcowXlYmRaGDskWkPgkxOwziYGM3mPEv8WaiP+Ec9BS1UugCcbA7TJON7RBJ6yHKyAB2QkXwB9NIn8+rrNsgcXQCmvgIrhdw2ny3gKbAcvo8OxME4FDbhtMWO0e1q3mf2ftDcrTLbntoKMlG0wSOEXwYF9aHXGo3Qeyj/Am8TIOf9hOXa/ValZRv7bfAg9Z/x6GsLNiT13l33KnIhV1pQ2aITuswTcSl18S7QoS1M+DgzAI7UE6uqXWj9ZNDX4NsnXRjpwSmpFMYqYG+8nSkZN+VIZZHTZ4opmqcassRjvyS7SaVDK4ZJvu/PSjSmstU9KhHrKffz9anlOCOXdt1lEN1t3pgwU25zRjigz6PTZD13FnUoBzxII6w3M/ydBcAC9sxnD6rQjoAfHAErquG7qRpAJ1HPSumvYj1jlV5QSWwFsrGK1K/sMGveOm+Z61K1OKhC5wf7yNZBZNI4jWfpD9gJ6arRTshl8gm/FwevvAH6P9NYd6koa9FU3THk7vIfAvP+S6Da006jE2CbJLMLdeIzXNcBrcpryt1NNI8nTvAL+X38Ov9c7VTv4WyPSfHmhoSKwIAZ1w70Cm63x1WEtsGTwHmW4QG8OaGe0RWgp665SNwy4Xwt8EehuRvYTlnstlRVB3nzFwu4BzEvzMvY2/MJdhWSKI661DL3duGjwCesTOrjFkFTyDV7BhdqcF6gycD21B+P/f/gatkZ1laeTSrAAAAABJRU5ErkJggg">
+                    <span>想看</span>
+                </div>
+                <div class="bow1_1">
+                    <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAAWBJREFUSA3tlr1OAkEUhcHAAygkavhp6Qy1vQUvxwOQWKKJD2DNE0AUCimsiFsYY2iM5fidZTbhL+wgXCCBk3yZmZ2798wdZslkMsg5V4ZHiMBKyi2PsjyzvvNC/0IPdqBvPG5UbRukZ4hXY2Gu3N6DxrVl/KEeqlgYTueUR+yEp7baaTKLpoOs+onfmZVBWt6TcdoObW3+8Laa09eAkacxX+qm8/qOYy1JLNNEo23NJwn3ttWrKjbZ6qTi0z/X/BkyG+/tcB2ncaQfkmO+k4uAPzSRtrrjBy3MLa8+KqzlvTqqtARfEKImQUtvKjxXnveAJPIqxQugo4vYA4Rcb++JmzmUjC/hDVZJdzt5TEx96akNL9zBD0hPkNdLtAXog9SD89Rk6waQ9BbGIOk6fA1dDdAAiuvmDI4neR0+QfqdNG5IexWc5L+BmNRAZtIrmH0NC2vELAdVmDloC4GH9uAPVr2hcd5RxioAAAAASUVORK5CYII">
+                    <span>评论</span>
+                </div>
+            </div>
+        </div>
+        <div class="qwe">
+            <div class="qwee">2019.08.10 19:30</div>
+            <div class="qwe1">
+                <p>国际体育馆（鸟巢）</p>
+                <img src="//static.piaoniu.com/nuxt-static/img/icon-park2.c5fced0.png">
+            </div>
+            <div class="qwe2">
+                <p>购票须知</p>
+                <span>></span>
+            </div>
+            <div class="qwe3">
+                <ul class="qwe3_1">
+                    <p>平台保障&nbsp;</p>
+                    <p>无票赔付&nbsp;</p>
+                    <p>出票保证&nbsp;</p>
+                    <p>配送保单</p>
+                    <span>></span>
+                </ul>
+            </div>
+            <div class="qweq">
+                <p>出价<br/>求购</p>
+            </div>
+            <div class="qwe4">
+                <div class="qwe41">
+                    <span>客服</span>
+                    <p @top="abc()">立即抢购</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        name: "Xqy",
+        methods:{
+            abc(){
+                this.$router.push('./Goupiao/index.vue')
+            }
+        }
+    }
+</script>
+
+<style scoped>
+.box{
+    position: absolute;
+    top: 170px;
+    width: 100%;
+}
+    .bod p span{
+        margin-left: 110px;
+    }
+    .boa{
+        position: absolute;
+        width: 100%;
+        background-color: #292138;
+        position: absolute;
+        overflow: hidden;
+        min-height: 180px;
+    }
+    .boa #poster{
+        float: left;
+        height: 128px;
+        width: 96px;
+        margin-left: 15px;
+        margin-top: 20px;
+    }
+    .boa1{
+        position: relative;
+        box-sizing: border-box;
+        margin: 20px 15px 16px 127px;
+        min-height: 128px;
+    }
+    .boa1_1{
+        font-size: 17px;
+        font-weight: 700;
+        min-height: 44px;
+        line-height: 22px;
+        color: #fff;
+        display: -webkit-box;
+        -webkit-line-clamp: 2;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        margin-bottom: 13px;
+    }
+    .boa1_2{
+        margin-bottom: 18px;
+        height: 20px;
+    }
+    .boa1_2 span{
+        font-size: 14px;
+        color: #fdb146;
+        margin-right: 6px;
+    }
+    .boa1_4{
+        display: -ms-flexbox;
+        display: flex;
+        -ms-flex-align: center;
+        align-items: center;
+        color: #fff;
+    }
+    .boa1_5{
+        -ms-flex: 1;
+        flex: 1;
+        display: -ms-flexbox;
+        display: flex;
+        -ms-flex-align: center;
+        align-items: center;
+    }
+    .boa1_5 .boa1_5_1{
+        font-size: 24px;
+        margin-right: 3px;
+    }
+    .boa1_5 .boa1_5_2{
+        font-size: 12px;
+    }
+    .boa1_6{
+        font-size: 10px;
+    }
+    .bow{
+        color: #fff;
+        margin: 16px 100px;
+    }
+    .bow img{
+        margin-bottom: -2px;
+        margin-right: 4px;
+    }
+    .bow span{
+        font-size: 14px;
+    }
+    .bow1{
+        float: right;
+    }
+    .qwe{
+        position: absolute;
+        top: 245px;
+        left: 20px;
+    }
+    .qwee{
+        font-size: 15px;
+        color: #222222;
+    }
+    .qwe1{
+        display: -ms-flexbox;
+        display: flex;
+        -ms-flex-align: center;
+        align-items: center;
+        height: 33px;
+        border-bottom: 1px solid #f5f5f5;
+        padding-bottom: 15px;
+        padding-top: 30px;
+    }
+    .qwe1 p{
+        margin-right: 11px;
+        overflow: hidden;
+        -ms-flex: 1;
+        flex: 1;
+        font-size: 15px;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        overflow: hidden;
+        color: #222222;
+    }
+    .qwe img{
+        margin-left: 170px;
+        width: 20px;
+        height: 24px;
+    }
+    .qwe2{
+        position: relative;
+        padding: 16px 0;
+        border-bottom: 1px solid #f5f5f5;
+    }
+    .qwe2 p{
+        font-size: 13px;
+        color: #333;
+    }
+    .qwe2 span{
+        position: absolute;
+        right: 7px;
+        margin-top: -7px;
+    }
+    .qwe3{
+        position: relative;
+        padding: 10px 0;
+        font-size: 14px;
+        border-bottom: 1px solid #f5f5f5;
+        display: -ms-flexbox;
+        display: flex;
+        width: 330px;
+        color: #222222;
+    }
+    .qwe3 p{
+        float: left;
+    }
+    .qwe3_1 span{
+        margin-left: 50px;
+        color: red;
+        font-size: 20px;
+    }
+    .qwe4{
+        width: 330px;
+        height: 40px;
+    }
+    .qwe41 span{
+        font-size: 14px;
+        color: #222222;
+        position: absolute;
+        bottom:-45px;
+    }
+    .qwe41 p{
+        color: #e9e9e9;
+        position: absolute;
+        right:-20px;
+        bottom:-56px;
+        width: 300px;
+        height: 40px;
+        background: #FF1493;
+        text-align: center;
+        line-height: 40px;
+    }
+    .qwe41 p a{
+        color: #e9e9e9;
+    }
+    .qweq{
+        position: fixed;
+        right: 10px;
+        width: 50px;
+        height: 50px;
+        background:#FF1493;
+        border-radius: 50%;
+        color: #e9e9e9;
+        font-size: 14px;
+        text-align: center;
+        line-height: 20px;
+    }
+</style>
